aa9c4733d8089aba6b52d1a1a32b8532fa9fbd31
syunsuke.shimizu <syunsuke.shimizu@gmail.com>
2016-07-10 22:05:17 +0900

    九九の表示を見やすくなるように修正しました。

--- 9x9/kuku.js
+++ 9x9/kuku.js
@@ -1,26 +1,28 @@
 var MAX = 9;
 
 document.write("<table>");
-document.write("<tbody>\n");
 for(var i=0;i<=MAX;i++){
   if(i==0) {
+    document.write("<thead>");
     document.write("<tr>");
     for(var ii=0; ii<=MAX; ii++) {
-      document.write("<td>"+ii+"</td>");
+      document.write("<th>"+ii+"</th>");
     }
     document.write("</tr>\n");
+    document.write("</thead>\n");
     continue;
   }
 
+  document.write("<tbody>\n");
   document.write("<tr>");
   for(var j=0;j<=MAX; j++){
     if(j==0) {
-      document.write("<td>"+i+"</td>");
+      document.write("<th scope='row'>"+i+"</th>");
     } else {
       document.write("<td>"+(i*j)+"</td>");
     }
   }
   document.write("</tr>\n");
+  document.write("</tbody>");
 }
-document.write("</tbody>");
 document.write("</table>");
